1d80344ce118124656155444228ec78d55cd37e6
Hamza BETBAIEB <hbetbaieb@finaxys.com>
2026-08-17 14:08:57 +0200

    Add riskboard-front as frontend folder

---
+++ riskboard/src/main/java/com/cacib/riskboard/RiskboardApplication.java
@@ -0,0 +1,13 @@
+package com.cacib.riskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskboardApplication.class, args);
+	}
+
+}
---
+++ riskboard/src/test/java/com/cacib/riskboard/RiskboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacib.riskboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
